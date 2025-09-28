80ce2ecce24a3540ff9352b9e78a6925cf29243d
omprakash970 <omprakashbandi583@gmail.com>
2025-09-28 12:22:37 +0530

    new commit

--- Src/app.js
+++ Src/app.js
@@ -1,7 +1,9 @@
 const App=()=>{
-    return React.createElement('div', {}, React.createElement('h1', {}, 'Hello World!') )
+    return React.createElement('div', {}, 
+        React.createElement('h1', {}, 'Hello World!') )
 }
 
 const container= document.getElementById('root');
-//new line added
+const root=ReactDOM.createRoot(container);
+root.render(React.createElement(App));
 
